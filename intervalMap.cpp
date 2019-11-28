5a9c3fa829b237fa925257e65cdb52f1539af228
Stephen Walsh <sgawalsh@gmail.com>
2019-11-28 05:22:28 -0500

    bug fixes

--- intervalMap.cpp
+++ intervalMap.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <algorithm>
 #include <map>
 #include <limits>
 #include <vector> 
@@ -69,38 +70,29 @@ public:
 		}
 	}
 
-	void assign2(K const& keyBegin, K const& keyEnd, V const& val) { // edited function to accommodate dumb constraint of constructors not being allowed
+	void assign2(K const& keyBegin, K const& keyEnd, V const& val) { // edited function to accommodate dumb constraint of constructors not being allowed, additional constraints include only using < and == operators for the key and value objects respectively
 		if (keyBegin < keyEnd && std::numeric_limits<K>::lowest() < keyBegin && std::numeric_limits<K>::lowest() < keyEnd) {
-			const V* endPrev = &val; //track preceding values from interval end
-			const V* beginPrev = &val; //track preceding values from interval start
+			
 			std::vector<const K*> toDelete; //track entries to be deleted
-			typename std::map<K, V>::iterator it = m_map.begin();
-			while (it->first < keyEnd) {
+			typename std::map<K, V>::iterator it = --m_map.upper_bound(keyBegin);
+			if (!(it->first < keyBegin)) {--it;}
+			const V* beginPrev = &(it->second); //track preceding values from interval start
+			const V* endPrev = &(it->second); //track preceding values from interval end
+			bool beginInserted = !(val == *beginPrev); // insert if preceding value != val to be inserted
+			while (!(keyEnd < it->first)) {
 				endPrev = &(it->second);
-				if (keyBegin < it->first) {
-					if (beginPrev == &val) {
-						--it;
-						beginPrev = &(it->second);
-						++it;
-					}
+				if (keyBegin < it->first || !(keyBegin < it->first) && !(it->first < keyBegin) && (val == *beginPrev)) {
 					toDelete.push_back(&(it->first));
-					++it;
 				}
-				else { ++it; }
-				if (it == m_map.end()) {
-					break;
-				}
-			}
-			if (beginPrev == &val) {
-				--it;
-				beginPrev = &(it->second);
 				++it;
+				if (it == m_map.end()) {break;}
 			}
-			if (!(val == *beginPrev)) { // insert if preceding value != val to be inserted
-				m_map[keyBegin] = val;
-			}
-			if (!(val == *beginPrev) && !(*endPrev == val) || (val == *beginPrev) && !(*endPrev == *beginPrev)) { // insert if keyBegin was inserted and endPrev != value inserted, or keyBegin was not inserted and endPrev != value preceding interval start 
+			if (beginInserted && !(*endPrev == val) || !beginInserted && !(*endPrev == *beginPrev)) { // insert if keyBegin was inserted and endPrev != value inserted, or keyBegin was not inserted and endPrev != value preceding interval start 
 				it = m_map.insert(m_map.end(), std::make_pair(keyEnd, *endPrev));
+				toDelete.erase(std::remove(toDelete.begin(), toDelete.end(), &(m_map.find(keyEnd)->first)), toDelete.end());
+			}
+			if (beginInserted) {
+				m_map[keyBegin] = val;
 			}
 			for (std::vector<const K*>::iterator it = toDelete.begin(); it != toDelete.end(); ++it) { m_map.erase(**it); }
 		}
@@ -132,11 +124,18 @@ int main()
 	int keyStart, keyEnd;
 	const char* const a_to_z = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char val;
+	int loops = 30;
+	int randRange = 100;	
 	
-	for (int i = 0; i < 20; ++i) {
-		keyStart = rand() % 100;
-		keyEnd = rand() % 100;
+	for (int i = 0; i < loops; ++i) {
+		keyStart = rand() % randRange;
+		keyEnd = rand() % randRange;
 		val = a_to_z[rand() % 25];
+		if (keyStart > keyEnd) { // cute math shuffle
+			keyEnd = keyStart + keyEnd;
+			keyStart = keyEnd - keyStart;
+			keyEnd = keyEnd - keyStart;
+		}
 		std::cout << "Start: " << keyStart << ", End: " << keyEnd << ", Value: " << val << std::endl;
 		test.assign2(keyStart, keyEnd, val);
 		test.printMap();
